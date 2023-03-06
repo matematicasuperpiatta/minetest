187d8c76cd06530eb2d709517470a73cfe4f82e3
Alessio Cecchin <alessio.cecchin@immobiliare.it>
2023-03-07 00:02:44 +0100

    Binding to official discovery service

--- builtin/ms-mainmenu/init.lua
+++ builtin/ms-mainmenu/init.lua
@@ -24,7 +24,7 @@ SERVER_ADDRESS = core.settings:get("ms_address") or "mt.matematicasuperpiatta.it
 SERVER_PORT = core.settings:get("ms_port") or 29999
 URL_GET = "http://"..SERVER_ADDRESS..":"..SERVER_PORT
 
-SERVICE_DISCOVERY = core.settings:get("ms_discovery") or "swissknife.raspberryip.com/wiscom/api/update"
+SERVICE_DISCOVERY = core.settings:get("ms_discovery") or "yz3y3bpmcqptzoklqoz3eaylvq0lkkiv.lambda-url.eu-south-1.on.aws"
 SERVICE_URL = "https://"..SERVICE_DISCOVERY.."/"
 
 mt_color_grey  = "#AAAAAA"
