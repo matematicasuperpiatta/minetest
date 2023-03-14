c914ded1ebfe5b88cc92f69f50002c206a728cdb
Alessio Cecchin <alessio.cecchin@immobiliare.it>
2023-03-14 23:55:31 +0100

    RC

--- builtin/ms-mainmenu/tab_ms.lua
+++ builtin/ms-mainmenu/tab_ms.lua
@@ -29,16 +29,21 @@ local function get_formspec(tabview, name, tabdata)
 
 	local fs = FormspecVersion:new{version=6}:render() ..
 	    -- Title
+		-- “image[2.2,0.3;7.68,3.17;” .. core.formspec_escape(defaulttexturedir .. “logo_320x132.png”)..“]”..
 		Image:new{
-			x=2.20, y=0.5, w=9.68, h=1.22,
+			x=2.20, y=-0.4, w=7.68, h=3.17,
+			path = defaulttexturedir .. "logo_320x132.png"}:render() ..
+		Image:new{
+			x=4.15, y=2.1, w=3, h=0.378,
 			path = defaulttexturedir .. "menu_header.png"}:render() ..
 
 		Image:new{
 			x=0.10, y=3.6, w=2, h=2,
 			path = defaulttexturedir .."univaq_block_image_small.png"}:render() ..
 
-		Label:new{x=2.4, y=0, label = fgettext("Basato su")}:render() ..
-		Label:new{x=2, y=4.5, label = fgettext("Spinoff dell'Università degli Studi dell'Aquila")}:render()
+		Label:new{x=4.9, y=1.7, label = fgettext("based on")}:render() ..
+		Label:new{x=2, y=4.1, label = fgettext("Università degli Studi of L'Aquila")}:render() ..
+		Label:new{x=2, y=4.5, label = fgettext("Spinoff")}:render()
 
 	if ms_mainmenu.remote.client_update.required then
 		-- Update
@@ -60,10 +65,10 @@ local function get_formspec(tabview, name, tabdata)
 			selectors = {"btn_mp_connect"},
 			props = {"bgcolor=#FF7F00", "font=bold", "alpha=false"}
 		}:render() ..
-			Button:new{x=9, y=4.2, w=2.5, h=1.75, name = "btn_mp_connect", label = fgettext("Connect")}:render()
+			Button:new{x=9, y=4.2, w=2.5, h=1.75, name = "btn_mp_connect", label = fgettext("Start")}:render()
 	end
 	return fs .. StyleType:new{selectors = {"label"}, props = {"font=italic"}}:render() ..
-	Label:new{x=2, y=4.9, label = fgettext("per informazioni: matematicasuperpiatta@gmail.com")}:render()
+	Label:new{x=2, y=4.9, label = fgettext("more info: matematicasuperpiatta@gmail.com")}:render()
 	-- .. ImageButton:new{x=3.6, y=4.9, w=0.6, h=0.6, path=defaulttexturedir .."envelope.png", name="btn_email"}:render()
 end
 
