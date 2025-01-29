d26bac69117a7d0ea745d24d3e5c5ba4c9f5b933
RobinSuperFlat <robin.short@stemblocks.it>
2025-01-29 11:19:29 +0100

    Update script to set local server

--- scr_set_server_local.py
+++ scr_set_server_local.py
@@ -2,32 +2,37 @@
 ATTENTION:  currently a reverse script to return to a remote server does not exist! Use carrefully.
 '''
 
-wiscom_local_address = "127.0.0.1:8000/wiscom"
-db_database = "wiscom_local"
+db_local_address = "127.0.0.1:8000/wiscom"
+db_database = "database_local"
 db_host = "127.0.0.1"
 db_password = "admin"
 
 
 ########################################################################################################################
-
+# ms_connect/init.lua
 with open("mods/ms/ms_connect/init.lua", "r") as file:
    lines = file.readlines()
 for i, line in enumerate(lines):
    if "matematicasuperpiatta.it/wiscom" in line:
-      lines[i] = line.split(" = ")[0] + ' = "' + wiscom_local_address + '/api/",\n'
+      lines[i] = line.split(" = ")[0] + ' = "' + db_local_address + '/api/",\n'
 with open("mods/ms/ms_connect/init.lua", "w") as file:
    for line in lines:
       file.write(line)
       
+# MTUser.py
 with open("mods/ms/raspberryjammod/ms_mcpipy/MTUser.py", "r") as file:
    lines = file.readlines()
 for i, line in enumerate(lines):
    if "wiscom_local = " in line:
       lines[i] = line.split(" = ")[0] + ' = True\n'
+   if i > 1 and 'if wiscom_local:' in lines[i-1]:
+      if "self._base_url = " in line:
+         lines[i] = line.split(" = ")[0] + ' = db_url_local\n'
 with open("mods/ms/raspberryjammod/ms_mcpipy/MTUser.py", "w") as file:
    for line in lines:
       file.write(line)
 
+# database.py
 with open("mods/ms/raspberryjammod/ms_mcpipy/database.py", "r") as file:
    lines = file.readlines()
 for i, line in enumerate(lines):
@@ -40,5 +45,13 @@ for i, line in enumerate(lines):
 with open("mods/ms/raspberryjammod/ms_mcpipy/database.py", "w") as file:
    for line in lines:
       file.write(line)
-      
-      
+  
+# block.py
+with open("mods/ms/raspberryjammod/mcpipy/mcpi/block.py", "r") as file:
+   lines = file.readlines()
+for i, line in enumerate(lines):
+   if "db_url = " in line:
+      lines[i] = line.split(" = ")[0] + ' = db_url_local\n'
+with open("mods/ms/raspberryjammod/mcpipy/mcpi/block.py", "w") as file:
+   for line in lines:
+      file.write(line)
