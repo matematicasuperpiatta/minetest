a760f662b98c9de30c4dcbea66930e72cfa9c3bf
RobinSuperFlat <robin.short@stemblocks.it>
2025-04-04 13:09:10 +0200

    Fix a bug in checkouts.sh

--- checkouts.sh
+++ checkouts.sh
@@ -1,15 +1,19 @@
 source versions.env
 
 # ms_shared
+echo "Try stash-checkout-pull-stash_pop on ms_shared:"$checkout_ms_shared
 cd mods/ms || { echo "❌ Errore: cartella mods/ms non trovata"; exit 1; }
 git stash
 git checkout "$checkout_ms_shared" || { echo "❌ Errore: checkout fallito su ms_shared"; exit 1; }
+git pull
 git stash pop
 cd ../../
 
 # ms_mcipipy
+echo "Try stash-checkout-pull-stash_pop on ms_mcpipy:"$checkout_ms_mcpipy
 cd mods/ms/raspberryjammod/ms_mcpipy || { echo "❌ Errore: cartella mods/ms/raspberryjammod/ms_mcpipy non trovata"; exit 1; }
 git stash
 git checkout "$checkout_ms_mcpipy" || { echo "❌ Errore: checkout fallito su ms_mcpipy"; exit 1; }
+git pull
 git stash pop
 cd ../../../../
