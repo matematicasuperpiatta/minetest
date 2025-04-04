7351f4a77ebacfb129a84c2dbd401e98479849c6
RobinSuperFlat <robin.short@stemblocks.it>
2025-04-04 12:45:15 +0200

    add run.sh script

---
+++ run.sh
@@ -0,0 +1,3 @@
+rm -f ./worlds/MATEMATICA_SUPERPIATTA/map.sqlite
+cp ./mods/ms/raspberryjammod/ms_mcpipy/MATEMATICA_SUPERPIATTA/map.sqlite ./worlds/MATEMATICA_SUPERPIATTA/
+./bin/minetestserver --world ./worlds/MATEMATICA_SUPERPIATTA/ --port 30000
