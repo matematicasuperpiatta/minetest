f2b0e5e6e44318eff50e5e8e1283057bc6ba07f3
RobinSuperFlat <robin.short@stemblocks.it>
2025-04-08 10:56:43 +0200

    Now checkout during cloning repositories is done using checkout command instead of -b option during clone command

--- clone.sh
+++ clone.sh
@@ -2,11 +2,19 @@
 
 source ./versions.env
 
+# clone and checkouts
 git clone --depth=1 -b 1.9.0mt8 https://github.com/minetest/irrlicht lib/irrlichtmt
-git clone -b "${checkout_ms_shared:-"develop"}" https://gitlab.com/leonardoguidoni/ms-shared.git mods/ms
-git clone -b "${checkout_ms_mcpipy:-"develop"}" https://gitlab.com/stemblocks/ms-mcpipy.git mods/ms/raspberryjammod/ms_mcpipy
-git clone -b "${checkout_minetest_game:-"bff7596364814e17f992f88a723a62957b29e69c"}" https://github.com/minetest/minetest_game.git games/minetest_game
 
+git clone https://gitlab.com/leonardoguidoni/ms-shared.git mods/ms
+cd mods/ms && git checkout "${checkout_ms_shared:-"develop"}" && cd ../../
+
+git clone https://gitlab.com/stemblocks/ms-mcpipy.git mods/ms/raspberryjammod/ms_mcpipy
+cd mods/ms/raspberryjammod/ms_mcpipy && git checkout "${checkout_ms_mcpipy:-"develop"}" && cd ../../../../
+
+git clone https://github.com/minetest/minetest_game.git games/minetest_game
+cd games/minetest_game && git checkout "${checkout_minetest_game:-"bff7596364814e17f992f88a723a62957b29e69c"}" && cd ../../
+
+# copies and deletions
 mkdir worlds
 cp -r mods/ms/raspberryjammod/ms_mcpipy/MATEMATICA_SUPERPIATTA worlds
 cd games/minetest_game/mods/ || exit
