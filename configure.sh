9b10ff75c40a8c94a906f62d40b5c787eddccce8
RobinSuperFlat <robin.short@stemblocks.it>
2025-04-08 12:35:04 +0200

    also wiscom_local information is copied into minetest.conf by configure.sh

--- configure.sh
+++ configure.sh
@@ -17,3 +17,18 @@ database_host = $db_host" >> "minetest.conf"
 fi
 
 echo "Configurato database_host nel file minetest.conf: $db_host"
+
+# Usa il valore di database_host da config.env o un valore di default
+wiscom_local="${wiscom_local:-default}"
+
+# Sovrascrivi il valore se già presente, altrimenti aggiungi la variabile al file minetest.conf
+if grep -q "^wiscom_local" "minetest.conf"; then
+    # Se la riga esiste, sovrascrivila con il nuovo valore
+    sed -i "s/^wiscom_local.*/wiscom_local = $wiscom_local/" "minetest.conf"
+else
+    # Se la riga non esiste, aggiungi la variabile al file
+    echo "
+wiscom_local = $wiscom_local" >> "minetest.conf"
+fi
+
+echo "Configurato wiscom_local nel file minetest.conf: $wiscom_local"
