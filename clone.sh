16f813f5089fa3f9348ba56613a61cdb8d5e8f72
RobinSuperFlat <robin.short@stemblocks.it>
2025-04-08 10:37:21 +0200

    now clone.sh do also checkouts

--- clone.sh
+++ clone.sh
@@ -3,10 +3,13 @@
 source ./versions.env
 
 git clone --depth=1 -b 1.9.0mt8 https://github.com/minetest/irrlicht lib/irrlichtmt
-git clone https://gitlab.com/leonardoguidoni/ms-shared.git mods/ms
-cd mods/ms/raspberryjammod || exit
-git clone https://gitlab.com/stemblocks/ms-mcpipy.git ms_mcpipy
-cd ms_mcpipy || exit
-mkdir ../../../../worlds
-cp -r MATEMATICA_SUPERPIATTA ../../../../worlds
-cd ../../../../
+git clone -b "${checkout_ms_shared:-"develop"}" https://gitlab.com/leonardoguidoni/ms-shared.git mods/ms
+git clone -b "${checkout_ms_mcpipy:-"develop"}" https://gitlab.com/stemblocks/ms-mcpipy.git mods/ms/raspberryjammod/ms_mcpipy
+git clone -b "${checkout_minetest_game:-"bff7596364814e17f992f88a723a62957b29e69c"}" https://github.com/minetest/minetest_game.git games/minetest_game
+
+mkdir worlds
+cp -r mods/ms/raspberryjammod/ms_mcpipy/MATEMATICA_SUPERPIATTA worlds
+cd games/minetest_game/mods/ || exit
+rm -rf beds binoculars boats bones bucket butterflies carts doors dungeon_lot env_sounds fireflies keysmap \
+       mtg_craftguide screwdriver sethome spawn vessels walls weather xpanes
+cd ../../../ || exit
